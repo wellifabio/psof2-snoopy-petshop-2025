5468ff5b639bc52ac2adf2a3ba12d646cfd35d23
wellifabio <wellifabio@hotmail.com>
2025-04-02 15:39:27 -0300

    front basico implementado

--- web/script.js
+++ web/script.js
@@ -49,6 +49,29 @@ fetch(dadosFront)
         });
     });
 
+//Preencher a tabela do Modal de listarClientes com os clientes obtidos da API
+function preencherClientes() {
+    const tbody = document.querySelector("#listarClientes tbody");
+    tbody.innerHTML = "";
+    clientes.forEach(cliente => {
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td data-label="Id">${cliente.id}</td>
+            <td data-label="CPF" contenteditable=true>${cliente.cpf}</td>
+            <td data-label="Nome" contenteditable=true>${cliente.nome}</td>
+            <td data-label="Telefones">
+                ${cliente.telefones.map(telefone => `${telefone.numero} (${telefone.tipo})<button onclick='removeTel(${telefone.id})'>-</button>`).join(", ")}
+                <button onclick="novoInput(this, ${cliente.id})">+</button>
+            </td>
+            <td data-label="Ações">
+                <button onclick="alteraCliente(this)">*</button>
+                <button onclick="removeCliente(${cliente.id})">-</button>
+            </td>
+        `;
+        tbody.appendChild(tr);
+    });
+}
+
 //Preencher a tabela do Modal de listarPedidos com os pedidos obtidos da API
 function preencherPedidos() {
     const tbody = document.querySelector("#listarPedidos tbody");
@@ -64,26 +87,7 @@ function preencherPedidos() {
             <td data-label="Quantidade">${pedido.qtd}</td>
             <td data-label="Preço">${pedido.preco}</td>
             <td data-label="Subtotal">${pedido.subTotal}</td>
-        `;
-        tbody.appendChild(tr);
-    });
-}
-
-//Preencher a tabela do Modal de listarClientes com os clientes obtidos da API
-function preencherClientes() {
-    const tbody = document.querySelector("#listarClientes tbody");
-    tbody.innerHTML = "";
-    clientes.forEach(cliente => {
-        const tr = document.createElement("tr");
-        tr.innerHTML = `
-            <td data-label="Id">${cliente.id}</td>
-            <td data-label="CPF">${cliente.cpf}</td>
-            <td data-label="Nome">${cliente.nome}</td>
-            <td data-label="Telefones">
-                ${cliente.telefones.map(telefone => `${telefone.numero} (${telefone.tipo})`).join(", ")}
-                <button onclick="novoInput(this, ${cliente.id})">+</button>
-            </td>
-            <td data-label="Ações"><button>*</button><button>-</button></td>
+            <td data-label="Excluir"><button onclick="removePedido(${pedido.id})">-</button></td>
         `;
         tbody.appendChild(tr);
     });
@@ -155,6 +159,65 @@ cadCli.addEventListener("submit", e => {
 
 });
 
+//Alterar dados do cliente
+function alteraCliente(elemento) {
+    const tr = elemento.parentNode.parentNode;
+    const id = tr.children[0].textContent;
+    const cpf = tr.children[1].textContent;
+    const nome = tr.children[2].textContent;
+
+    const dados = {
+        nome: nome,
+        cpf: cpf != "" ? cpf : null
+    };
+
+    fetch(uri + "/clientes/" + id, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dados)
+    }).then(resp => resp.status)
+        .then(status => {
+            if (status == 202) {
+                alert("Cliente alterado com sucesso!");
+            } else {
+                alert("Erro ao alterar cliente!");
+            }
+            window.location.reload();
+        });
+}
+
+//Excluir cliente
+function removeCliente(id) {
+    fetch(uri + "/clientes/" + id, {
+        method: "DELETE"
+    }).then(resp => resp.status)
+        .then(status => {
+            if (status == 204) {
+                alert("Cliente removido com sucesso!");
+            } else {
+                alert("Erro ao remover cliente!");
+            }
+            window.location.reload();
+        });
+}
+
+//Função para remover telefone do cliente
+function removeTel(id) {
+    fetch(uri + "/telefones/" + id, {
+        method: "DELETE"
+    }).then(resp => resp.status)
+        .then(status => {
+            if (status == 204) {
+                alert("Telefone removido com sucesso!");
+            } else {
+                alert("Erro ao remover telefone!");
+            }
+            window.location.reload();
+        });
+}
+
 //Modal de pedidos
 function abrirModalPedido(id) {
     const produto = produtos.find(produto => produto.id == id);
@@ -213,4 +276,19 @@ function abrirModalPedido(id) {
             });
 
     });
+}
+
+//Remove pedido
+function removePedido(id) {
+    fetch(uri + "/pedidos/" + id, {
+        method: "DELETE"
+    }).then(resp => resp.status)
+        .then(status => {
+            if (status == 204) {
+                alert("Pedido removido com sucesso!");
+            } else {
+                alert("Erro ao remover pedido!");
+            }
+            window.location.reload();
+        });
 }
